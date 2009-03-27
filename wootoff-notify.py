28425d6b68127859fd014f1c0449f40d64ff5afb
James Wilson <github@meswilson.com>
2009-03-27 04:26:29 -0500

    woot off notifier

---
+++ wootoff-notify.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+wait=15
+
+import pynotify
+import time
+import urllib
+
+
+if not pynotify.init ("icon-summary-body"):
+	sys.exit(1)
+
+
+cur = '' # current item
+so = False # sold out
+
+def notify(usr,message,icon="notification-message-IM"):
+	n = pynotify.Notification (usr,message,icon)
+	n.show()
+
+while 1:
+	try:
+		r = urllib.urlopen('http://www.woot.com/DefaultMicrosummary.ashx').read()
+	except:
+		pass
+	else:
+		data = r.split(' : ')
+		name = data[2]
+		price = data[1]
+		percent = data[0]
+		out = "SOLD OUT" in r
+		if name != cur or (not so and out):
+			notify(name, "%s - %s" % (price, percent))
+			cur = name
+			so = out
+	time.sleep(wait)
+
